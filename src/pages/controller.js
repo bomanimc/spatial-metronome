27a95bec1e3a33978109f769268cc85c5a1403ab
Bomani Oseni McClendon <bomanimc@gmail.com>
2022-02-08 19:22:32 -0500

    Add some text to the controller page to indicate state

--- src/pages/controller.js
+++ src/pages/controller.js
@@ -9,22 +9,25 @@ import Seo from "../components/seo";
 const socket = io(process.env.GATSBY_SOCKET_SERVER_URL);
 
 const ControllerPage = () => {
-
+  const [text, setText] = useState("Ticking...");
   
   const onMouseDown = () => {
     console.log('Setting Quiet');
     socket.emit("quiet");
+    setText("Silenced.");
   }
 
   const onMouseUp = () => {
     console.log('Setting Loud');
     socket.emit("loud");
+    setText("Ticking...");
   }
 
   return (
     <Layout>
       <Seo title="Home" />
       <ControllerPage.Button onMouseDown={onMouseDown} onMouseUp={onMouseUp}>Click to Silence</ControllerPage.Button>
+      <ControllerPage.StateDetail>{text}</ControllerPage.StateDetail>
     </Layout>
   );
 }
@@ -37,6 +40,14 @@ ControllerPage.ColorBackground = styled.div`
   align-items: center;
   justify-content: flex-end;
   padding: 1rem;
+`;
+
+ControllerPage.StateDetail = styled.div`
+  color: white;
+  position: fixed;
+  bottom: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
 `;
 
 ControllerPage.Button = styled(Button)``;
