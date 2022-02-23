52b8836318e0b55c133987023bba817dc221d2f2
Bomani Oseni McClendon <bomanimc@gmail.com>
2022-02-22 20:54:42 -0500

    Update controller page design

--- src/pages/controller.js
+++ src/pages/controller.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import { io } from "socket.io-client";
 import { Button } from '@bomanimc/system';
@@ -8,50 +8,42 @@ import Seo from "../components/seo";
 
 const socket = io(process.env.GATSBY_SOCKET_SERVER_URL);
 
+const DEFAULT_TEXT_STATE = "They are allowed to pulse.";
+
 const ControllerPage = () => {
-  const [text, setText] = useState("Ticking...");
+  const [text, setText] = useState(DEFAULT_TEXT_STATE);
   
   const onMouseDown = () => {
     console.log('Setting Quiet');
     socket.emit("quiet");
-    setText("Silenced.");
+    const buttonRef = document.getElementById('screenButton');
+    buttonRef.style.background = 'white';
+    buttonRef.style.color = 'black';
+    setText("You've silenced them all.");
   }
 
   const onMouseUp = () => {
     console.log('Setting Loud');
     socket.emit("loud");
-    setText("Ticking...");
+    const buttonRef = document.getElementById('screenButton');
+    buttonRef.style.background = 'black';
+    buttonRef.style.color = 'white';
+    setText(DEFAULT_TEXT_STATE);
   }
 
   return (
     <Layout>
       <Seo title="Home" />
-      <ControllerPage.Button onMouseDown={onMouseDown} onMouseUp={onMouseUp}>Click to Silence</ControllerPage.Button>
-      <ControllerPage.StateDetail>{text}</ControllerPage.StateDetail>
+      <ControllerPage.Button id="screenButton" onPointerDown={onMouseDown} onPointerUp={onMouseUp}>{text}</ControllerPage.Button>
     </Layout>
   );
 }
 
-ControllerPage.ColorBackground = styled.div`
-  flex: 1;
-  background: ${p => p.bgColor};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
-  padding: 1rem;
-`;
-
-ControllerPage.StateDetail = styled.div`
-  color: white;
-  position: fixed;
-  bottom: 1rem;
-  left: 50%;
-  transform: translateX(-50%);
-`;
-
 ControllerPage.Button = styled(Button)`
-  font-family: inherit;
+  border: none;
+  font-size: 8vw;
+  padding: 1rem;
+  transition: color .25s ease-in-out, background .25s ease-in-out;
 `;
 
 export default ControllerPage;
