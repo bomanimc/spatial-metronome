7cee81c65ecb450a4c6cf96afef1aceee40b5eb5
Bomani Oseni McClendon <bomani@duolingo.com>
2022-08-21 19:30:03 +0200

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Whitened Noise`,
+    description: `Collectively silencing a apatial metronomic sound.`,
+    author: `@bomanimc`,
+    siteUrl: `https://whitenednoise.bomani.rip/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,15 +25,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'Whitened Noise',
+        short_name: 'Whitened Noise',
+        start_url: '/',
+        background_color: '#000',
+        theme_color: '#000',
+        display: 'minimal-ui',
+        icon: 'src/images/drip.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
